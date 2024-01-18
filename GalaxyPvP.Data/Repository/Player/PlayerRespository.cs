c358ed1395c8ffa5574b8ad1823be213be99fc93
AngchonLee <72794028+desertfoxgame@users.noreply.github.com>
2024-01-18 09:08:55 +0700

    [feat] player respository

--- GalaxyPvP.Data/Repository/Player/PlayerRespository.cs
+++ GalaxyPvP.Data/Repository/Player/PlayerRespository.cs
@@ -1,14 +1,18 @@
-﻿using GalaxyPvP.Data.Context;
+﻿using AutoMapper;
+using GalaxyPvP.Data.Context;
 using GalaxyPvP.Data.Model;
 
 namespace GalaxyPvP.Data
 {
     public class PlayerRespository : GenericRepository<Player>, IPlayerRepository
     {
-        private GalaxyPvPContext _dbContext;
-        protected PlayerRespository(GalaxyPvPContext context) : base(context)
+        private GalaxyPvPContext _db;
+        private readonly IMapper _mapper;
+
+        public PlayerRespository(GalaxyPvPContext db,IMapper mapper):base(db)
         {
-            _dbContext = context;
+            _db = db;
+            _mapper = mapper;
         }
 
     }
