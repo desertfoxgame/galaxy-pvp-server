731f6e850f84117188841be780ad6973cfbd4f4d
AngchonLee <72794028+desertfoxgame@users.noreply.github.com>
2024-01-22 16:03:54 +0700

    [feat] fix extension

---
+++ GalaxyPvP.Helper/GenerateExtension.cs
@@ -0,0 +1,19 @@
+﻿using NanoidDotNet;
+using System.Security.Cryptography;
+
+
+namespace GalaxyPvP.Extensions
+{
+    public class GenerateExtension
+    {
+        public static string GenerateRandomCode(int length)
+        {
+            return Nanoid.Generate("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length);
+        }
+
+        public static string GeneratePassword(int length)
+        {
+            return Nanoid.Generate("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?", length);
+        }
+    }
+}
