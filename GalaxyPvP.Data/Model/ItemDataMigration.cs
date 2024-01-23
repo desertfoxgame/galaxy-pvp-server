4ba194f315d364ebac46ce065b74c8f7bdc904ef
TinhLD <59137707+TinhLeDanh@users.noreply.github.com>
2024-01-23 11:41:03 +0700

    ignore

--- GalaxyPvP.Data/Model/ItemDataMigration.cs
+++ GalaxyPvP.Data/Model/ItemDataMigration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace GalaxyPvP.Data.Model
     public class ItemDataMigration
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public int DataId { get; set; }
         public string Name { get; set; }
     }
 }
