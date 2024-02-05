5d027084ebc2a074748fe2f94e753654a901c064
HoangNguyen90 <92721153+HoangNguyen90@users.noreply.github.com>
2024-02-05 17:21:07 +0700

    [fix] rootpath

--- GalaxyPvP.Helper/EmailExtension.cs
+++ GalaxyPvP.Helper/EmailExtension.cs
@@ -20,7 +20,7 @@ namespace GalaxyPvP.Extensions
 
         public static async Task SendGridEmailAsync(string recipient, string subject, string body)
         {
-            string htmlTemplatePath = Directory.GetCurrentDirectory() + "\\emailTemplate.json";
+            string htmlTemplatePath = Directory.GetCurrentDirectory() + "\\wwwroot\\emailTemplate.json";
             string htmlContentTemplate = File.ReadAllText(htmlTemplatePath);
 
             // Replace the placeholder with the actual body content
